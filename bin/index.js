b3456140d49d45d93e4d4844fdb5fb8992601e8f
Daniel Ortiz Lira <dortizlira@gmail.com>
2023-10-04 08:36:55 -0500

    feat: index objects and delete playlists

--- bin/index.js
+++ bin/index.js
@@ -22,6 +22,18 @@ if (!fs.pathExistsSync(changesFilePath)) {
   handleInvalidInput(changesFilePath);
 }
 
+const indexerReducer = (acc, { id, ...rest }) => ({ ...acc, [id]: rest });
+
 const { users, playlists, songs } = fs.readJSONSync(inputFilePath);
 const { add_songs_to_playlist, new_playlists, deleted_playlists } =
   fs.readJSONSync(changesFilePath);
+
+// We traverse the objects once to add the id as an index.
+// This way we only need to traverse them once.
+const indexedSongs = songs.reduce(indexerReducer, {});
+const indexedUsers = users.reduce(indexerReducer, {});
+
+// Delete playlists
+const indexedPlaylists = playlists.reduce((acc, { id, ...rest }) => {
+  return deleted_playlists[id] === true ? acc : { ...acc, [id]: rest };
+}, {});
